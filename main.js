fa3be719fb33cbc3d43d5b3d5905f9e78a4dd983
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2018-06-17 16:47:23 +0200

    HTTP Basic Auth

--- main.js
+++ main.js
@@ -8,18 +8,26 @@ const upload = require("./controllers/upload");
 const download = require("./controllers/download");
 const downloadTorrent = require("./controllers/downloadTorrent");
 
+const auth = require("http-auth");
 const multer = require("multer");
+const config = require("./config.json");
 
 app.set("view engine","ejs");
 app.set("views",__dirname + "/views");
+
+let basic = auth.basic({
+    realm: "Upload PPT"
+}, function (username, password, callback) {
+    callback(username === config.username && password === config.password);
+});
 
 app.use("/static",express.static("static"));
 
 app.get("/",index);
 app.get("/download/:id",download);
 app.get("/downloadTorrent/:id",downloadTorrent);
-//app.get("/upload",upload.get);
-//app.post("/upload",multer({dest: "./static/"}).any(),upload.post);
+app.get("/upload",auth.connect(basic),upload.get);
+app.post("/upload",auth.connect(basic),multer({dest: "./static/"}).any(),upload.post);
 
 app.listen(5454,function(){
     db.sync()
