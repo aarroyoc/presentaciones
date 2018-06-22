ab919deccd55214d0013d75c7cbc08b62147f541
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2018-06-22 21:38:44 +0200

    Allow main.js to be imported

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const app = express();
 
 const db = require("./db");
 
@@ -12,31 +11,45 @@ const auth = require("http-auth");
 const multer = require("multer");
 const config = require("./config.json");
 
-app.set("view engine","ejs");
-app.set("views",__dirname + "/views");
+function setup(){
+    const app = express();
+    app.set("view engine","ejs");
+    app.set("views",__dirname + "/views");
 
-let basic = auth.basic({
-    realm: "Upload PPT"
-}, function (username, password, callback) {
-    callback(username === config.username && password === config.password);
-});
+    let basic = auth.basic({
+        realm: "Upload PPT"
+    }, function (username, password, callback) {
+        callback(username === config.username && password === config.password);
+    });
 
-app.use("/static",express.static("static"));
+    app.use("/static",express.static("static"));
 
-app.get("/",index);
-app.get("/download/:id",download);
-app.get("/downloadTorrent/:id",downloadTorrent);
-app.get("/upload",auth.connect(basic),upload.get);
-app.post("/upload",auth.connect(basic),multer({dest: "./static/"}).any(),upload.post);
+    app.get("/",index);
+    app.get("/download/:id",download);
+    app.get("/downloadTorrent/:id",downloadTorrent);
+    app.get("/upload",auth.connect(basic),upload.get);
+    app.post("/upload",auth.connect(basic),multer({dest: "./static/"}).any(),upload.post);
 
-app.listen(5454,function(){
-    db.sync()
-        .then(()=>{
-            console.log("Presentaciones running at 5454");
-        })
-        .catch((e)=>{
-            console.error("Error with Sequelize-MariaDB");
-            console.error(e);
-        });
-    
-});
+    return app;
+}
+
+if(require.main === module){
+    let app = setup();
+
+    app.listen(5454,function(){
+        db.sync()
+            .then(()=>{
+                console.log("Presentaciones running at 5454");
+            })
+            .catch((e)=>{
+                console.error("Error with Sequelize-MariaDB");
+                console.error(e);
+            });
+        
+    });
+}
+
+
+module.exports = {
+    setup: setup
+};
